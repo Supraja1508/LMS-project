bbb4eb97ccc73e591d2461205e98aeab993e424d
Supraja1508 <144414241+Supraja1508@users.noreply.github.com>
2025-07-29 21:16:37 +0530

    fontend

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Hero from '../../components/student/hero';
+import Hero from '../../components/student/Hero';
 import Companies from '../../components/student/Companies';
 import CoursesSection from '../../components/student/CoursesSection';
 import TestimonialsSection from '../../components/student/TestimonialsSection';
